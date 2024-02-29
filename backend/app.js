75c2cd4b0bc8eb33a73eceef5f8c24724fccacb8
Murat Yıldırım <2020555068@ogr.cu.edu.tr>
2024-02-29 22:38:28 +0300

    Added reservation router and error middleware.

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from "cors";
 import dotenv from 'dotenv'
 import { dbConnection } from './database/dbConnection.js';
+import {errorMiddleware} from "./error/error.js"
+import reservationRouter from "./routes/reservationRoute.js"
 
 
 const app = express( )
@@ -15,7 +17,10 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(`/api/v1/reservation`, reservationRouter)
 
 dbConnection();
+
+app.use(errorMiddleware)
 
 export default app;
