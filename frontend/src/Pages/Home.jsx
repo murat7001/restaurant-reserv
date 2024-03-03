371f9ca22df64b02b8d079467bba047d2d9a70a6
Murat Yıldırım <2020555068@ogr.cu.edu.tr>
2024-03-03 19:12:33 +0300

    Add HeroSection component to Home page

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HeroSection from '../components/HeroSection'
 
 function Home() {
   return (
     <>
-        <h1>Home page</h1>
+      <HeroSection></HeroSection>
     </>
   )
 }
