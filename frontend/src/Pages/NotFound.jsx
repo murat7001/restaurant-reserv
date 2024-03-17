63bd05640e300f0b8917021613ded7b560738a98
Murat Yıldırım <2020555068@ogr.cu.edu.tr>
2024-03-17 17:06:35 +0300

    Update NotFound page with new layout and content

--- frontend/src/Pages/NotFound.jsx
+++ frontend/src/Pages/NotFound.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { HiOutlineArrowRight } from 'react-icons/hi'
 
 const NotFound = () => {
   return (
     <>
-        <h1>Not Found Page</h1>
+      <section className='notFound' id='notFound'>
+        <div className="container">
+          <img src="/notFound.svg" alt="notFound" />
+          <h1>Look like you are lost</h1>
+          <p>We can not seem to find the page you are looking for.</p>
+          <Link to={'/'}>Home <span><HiOutlineArrowRight /></span></Link>
+        </div>
+      </section>
     </>
   )
 }
